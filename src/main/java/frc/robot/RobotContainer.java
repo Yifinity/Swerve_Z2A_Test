29b93f56bd7eb3cdb691fa1fa5b5c67f62ec6fc4
Yifan Jiang <yifan99.jiang@gmail.com>
2023-12-24 22:18:15 -0500

    i don't know what's going on

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,11 @@ package frc.robot;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.SwerveJoystickCMD;
 import frc.robot.subsystems.SwerveSubsystem;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -21,16 +22,15 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
 public class RobotContainer {
@@ -45,6 +45,14 @@ public class RobotContainer {
                 () -> true/*
                 () -> !xbox.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx */);
 
+  // Autonomous Commands
+  String onePieceJSON = "output/OnePiece.wpilib.json";
+  String onePieceReturnJSON = "output/OnePieceReturn.wpilib.json";
+  Trajectory selectedPath = new Trajectory();
+
+ 
+
+
   public RobotContainer() {
     CommandScheduler.getInstance().setDefaultCommand(swerveSubsystem, swerveCMD);
     configureBindings();
@@ -57,24 +65,33 @@ public class RobotContainer {
   }
 
   
-  
   public Command getAutonomousCommand() {
-    // create trajectory settings
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared).setKinematics(DriveConstants.kDriveKinematics);
-    // generate trajectory
-    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)), 
-    List.of(
-      new Translation2d(1, 0),
-      new Translation2d(1, -1)), 
-      new Pose2d(2, -1, Rotation2d.fromDegrees(180)), 
-      trajectoryConfig);
-      // define pid controllers for tracking trajectory
       PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
       PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
       ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-      // contruct command to follow trajectory
-      SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-        trajectory, 
+    
+      try {
+        Path onePiecePath = Filesystem.getDeployDirectory().toPath().resolve(onePieceJSON);
+        Trajectory onePieceTraj = TrajectoryUtil.fromPathweaverJson(onePiecePath);
+        selectedPath = onePieceTraj;
+    
+     } catch (IOException ex) {
+        DriverStation.reportError("Unable to open trajectory: " + onePieceJSON, ex.getStackTrace());
+
+        TrajectoryConfig trajectoryConfig = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared).setKinematics(DriveConstants.kDriveKinematics);
+          // generate trajectory
+        selectedPath = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)), 
+        List.of(
+          new Translation2d(1, 0),
+          new Translation2d(1, -1)), 
+          new Pose2d(2, -1, Rotation2d.fromDegrees(180)), 
+          trajectoryConfig);
+     }
+
+
+    // create command to follow trajectory
+    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+      selectedPath, 
         swerveSubsystem::getPose, 
         DriveConstants.kDriveKinematics, 
         xController, 
@@ -82,9 +99,11 @@ public class RobotContainer {
         thetaController,
         swerveSubsystem::setModuleStates,
         swerveSubsystem);
+
+
       // add some init and wrap up, and return everything
     return new SequentialCommandGroup(
-      new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
+      new InstantCommand(() -> swerveSubsystem.resetOdometry(selectedPath.getInitialPose())),
       swerveControllerCommand,
       new InstantCommand(() -> swerveSubsystem.stopModules())
     );
